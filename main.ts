a163f010545a5486eeaa2db7745f7973dd9d1854
Nanogamexe <47512743+Nanogamexe@users.noreply.github.com>
2021-11-22 09:44:06 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
+let intensite = 0
+function purple () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
 function green () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
 }
 function blue () {
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -14,5 +25,16 @@ function red () {
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
 basic.forever(function () {
-	
+    intensite = pins.analogReadPin(AnalogPin.P0)
+    if (intensite <= 700) {
+        purple()
+    } else if (intensite < 700 && intensite >= 500) {
+        red()
+    } else if (intensite < 500 && intensite >= 300) {
+        blue()
+    } else if (intensite < 300 && intensite >= 100) {
+        green()
+    } else {
+        blanc()
+    }
 })
